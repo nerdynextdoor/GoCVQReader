a4fad74bc916ad81fd87f27daa7f7d833eba07e9
nerdynextdoor <73428843+nerdynextdoor@users.noreply.github.com>
2020-12-26 01:33:34 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"bytes"
 	"fmt"
 	"image"
+	"image/color"
 	_ "image/jpeg"
 	_ "image/png"
 	"io/ioutil"
 	"log"
+	"math"
 	"time"
+
+	"github.com/llgcode/draw2d/draw2dimg"
+	"gonum.org/v1/plot"
 
 	"github.com/liyue201/goqr"
 )
@@ -40,10 +45,112 @@ func recognizeFile(path string) {
 	}
 	elapsed := time.Since(start)
 	log.Printf("Binomial took %s", elapsed)
+	fmt.Printf("globel Test  %v \n", goqr.GV)
 
+	//p1.x
+	angleTest(goqr.Gx1, goqr.Gy1, goqr.Gx2, goqr.Gy2)
+	//angleTest(goqr.GV[0],)
 	//fmt.Printf("image.Decode error: %v\n", goqr.)
 }
 
 func main() {
 	recognizeFile("QRgrid.png")
+
+	//drawing2()
+}
+
+type Position struct {
+	X float64
+	Y float64
+}
+
+func angleTest(x1, y1, x2, y2 float64) {
+	ydiff := y2 - y1
+	xdiff := x2 - x1
+	radians := math.Atan2(ydiff, xdiff)
+	fmt.Printf("Ydiff: %v\n", ydiff)
+	fmt.Printf("Xdiff: %v\n", xdiff)
+	fmt.Printf("radians: %v\n", radians)
+
+	angle := radians * 180 / math.Pi
+
+	fmt.Printf("Angle: %v\n", angle)
+
+}
+
+func drawing2() {
+	// Initialize the graphic context on an RGBA image
+	dest := image.NewRGBA(image.Rect(0, 0, 600, 600))
+	gc := draw2dimg.NewGraphicContext(dest)
+
+	// Set some properties
+	gc.SetFillColor(color.RGBA{0x44, 0xff, 0x44, 0xff})
+	gc.SetStrokeColor(color.RGBA{0x44, 0x44, 0x44, 0xff})
+	gc.SetLineWidth(5)
+
+	// Draw a closed shape
+	gc.MoveTo(314, 258) // should always be called first for a new path
+	//gc.LineTo(342, 258)
+	//gc.QuadCurveTo(341, 258, 314, 285)
+	gc.CubicCurveTo(341, 258, 341, 385, 314, 258)
+	gc.Close()
+	gc.FillStroke()
+
+	// Save to file
+	draw2dimg.SaveToPngFile("./hello.png", dest)
+}
+
+func drawing() {
+	p, err := plot.New()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	//plotter.DefaultLineStyle.Width = vg.Points(1)
+	//plotter.DefaultGlyphStyle.Radius = vg.Points(3)
+
+	//p.Y.Tick.Marker = plot.ConstantTicks([]plot.Tick{
+	//	{Value: 0, Label: "0"}, {Value: 0.25, Label: ""}, {Value: 0.5, Label: "0.5"}, {Value: 0.75, Label: ""}, {Value: 1, Label: "1"},
+	//})
+	//p.X.Tick.Marker = plot.ConstantTicks([]plot.Tick{
+	//	{Value: 0, Label: "0"}, {Value: 0.25, Label: ""}, {Value: 0.5, Label: "0.5"}, {Value: 0.75, Label: ""}, {Value: 1, Label: "1"},
+	//})
+
+	//pts := plotter.XYs{{X: 0, Y: 0}, {X: 0, Y: 1}, {X: 0.5, Y: 1}, {X: 0.5, Y: 0.6}, {X: 0, Y: 0.6}}
+	//line, err := plotter.NewLine(pts)
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//scatter, err := plotter.NewScatter(pts)
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//p.Add(line, scatter)
+
+	//pts = plotter.XYs{{X: 1, Y: 0}, {X: 0.75, Y: 0}, {X: 0.75, Y: 0.75}}
+	//line, err = plotter.NewLine(pts)
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//scatter, err = plotter.NewScatter(pts)
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//p.Add(line, scatter)
+
+	//pts = plotter.XYs{{X: 0.5, Y: 0.5}, {X: 1, Y: 0.5}}
+	//line, err = plotter.NewLine(pts)
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//scatter, err = plotter.NewScatter(pts)
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	//p.Add(line, scatter)
+
+	err = p.Save(100, 100, "./plotLogo.png")
+	if err != nil {
+		log.Panic(err)
+	}
 }
